3040a6e3b066f352af33fc2cb5b70534741461bb
CLC <laichwang@gmail.com>
2024-07-24 10:34:47 +0800

    Update main.tf with backend block

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
+#============ MAIN =============
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+# To comment backend block if working on local and terraform state file is locally stored
+  backend "s3" {
+    bucket = "sctp-ce7-tfstate" 
+    key    = "terraform-ex-ec2-lcchua.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
+    region = "us-east-1"
   }
 }
 
