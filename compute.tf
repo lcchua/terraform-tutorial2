aab9d0b6ec0837203bb7304b7987bef43226b6b4
lcchua <laichwang@gmail.com>
2024-07-25 14:37:47 +0800

    Added new EC2 key pair creation and bootstrap installation of httpd and docker

--- compute.tf
+++ compute.tf
@@ -28,9 +28,22 @@ resource "aws_instance" "lcchua-tf-ec2" {
   instance_type = "t2.micro"
 
   key_name      = var.key_name
+#  key_name      = aws_key_pair.lcchua-tf-key-pair.key_name
   subnet_id     = aws_subnet.lcchua-tf-public-subnet-az1.id
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.lcchua-tf-sg-allow-ssh-http-https.id]
+
+# User data script to convert the EC2 server into a HTTPD web server
+#  user_data = <<EOF
+#    #!/bin/bash
+#    echo "Installing the httpd and docker packages to the EC2 server..."
+#    yum update -y
+#    yum install httpd -y
+#    yum install docker -y
+#    systemctl start httpd.service
+#    systemctl enable httpd.service
+#    echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+#  EOF
 
   tags = {
     group = var.stack_name
