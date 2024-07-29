64661841f6cd91a7d9264ea0612109f947004612
lcchua <laichwang@gmail.com>
2024-07-29 12:40:08 +0800

    Uncommented httpd, docker installation lines for testing

--- ws_install.sh
+++ ws_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "Installing the httpd and docker packages to the EC2 server..." > /tmp/ec2-user.txt
 
-<<COMMENT_OUT
 yum update -y
 yum install httpd -y
 yum install docker -y
@@ -14,4 +12,3 @@ find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
 echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
 reboot
-COMMENT_OUT
